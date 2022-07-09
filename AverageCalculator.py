195dff755b0ff2fe2ffeca4bd06668375ea67b2f
Rl242Dev <robin.lachand@gmail.com>
2022-07-09 14:48:50 +0200

    [UPDATE] Adding Graph + Float Grade Feature

--- AverageCalculator.py
+++ AverageCalculator.py
@@ -1,4 +1,6 @@
 from time import sleep
+import matplotlib.pyplot as plt
+import numpy as np
 
 NumberGrade = ['First', 'Seconde','Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh', 'Eighth', 'Ninth', 'Tenth', 'Eleventh', 'Twelfth', 'Thirteenth', 'Fourteenth', 'Fifteenth', 'Sixteenth', 'Seventeenth', 'Eighteenth', 'Nineteenth', 'Twentieth'] # Max number of Grade
 
@@ -13,13 +15,21 @@ if NumberOfGradeInput < 2:
 else:
     x = 0
     GradeList = []
+    PlotNumberGrade = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
     while True:
         if x == NumberOfGradeInput:
             TotalGrade = sum(GradeList)
             TotalAverage = TotalGrade / len(GradeList)
             print('You have an average of :', TotalAverage)
+            while len(PlotNumberGrade) != len(GradeList):
+                PlotNumberGrade = np.delete(PlotNumberGrade, -1)
+            plt.scatter(PlotNumberGrade, GradeList)
+            plt.xlabel('X - Number of Grade')
+            plt.ylabel('Y - Grade')
+            plt.title('Average Graph')
+            plt.show()
             break
-        Grade = int(input(NumberGrade[x]+': '))
+        Grade = float(input(NumberGrade[x]+': '))
         if Grade > 20:
             print('Error, Max Input 20')
             quit()
